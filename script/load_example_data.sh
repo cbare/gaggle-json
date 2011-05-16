0ef714ef6f8eb6fa622c85f230e701f0eef37ef3
Christopher Bare <christopherbare@gmail.com>
2011-05-15 22:55:37 -0700

    combined sample data scripts

--- script/load_example_data.sh
+++ script/load_example_data.sh
@@ -7,3 +7,9 @@ curl -v --request PUT -H "Content-Type: application/json" --upload-file tuple.bi
 curl -v --request PUT -H "Content-Type: application/json" --upload-file tuple.command.json http://127.0.0.1:3000/documents/4da613fc1ff236120b000008
 curl -v --request PUT -H "Content-Type: application/json" --upload-file table.json http://127.0.0.1:3000/documents/4da614391ff236120b000009
 cd ../..
+curl --request PUT http://localhost:3000/projects/4db9e0b557305b6adc000002 --data '{"name":"Test project", "description":"A project holding example Gaggle data objects"}'
+curl --request PUT http://localhost:3000/projects/4db9e0b557305b6adc000002/documents/4da60f351ff236120b000003
+curl --request PUT http://localhost:3000/projects/4db9e0b557305b6adc000002/documents/4da60fd51ff236120b000004
+curl --request PUT http://localhost:3000/projects/4db9e0b557305b6adc000002/documents/4da612601ff236120b000005
+curl --request PUT http://localhost:3000/projects/4db9e0b557305b6adc000002/documents/4da613a91ff236120b000006
+curl --request PUT http://localhost:3000/projects/4db9e0b557305b6adc000002/documents/4da614391ff236120b000009
